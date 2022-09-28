f2eeed182b7480e21354f2c0d17c21f006b987e1
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-09-28 19:43:31 +0000

    updated Scratch Pad D2

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -13,7 +13,26 @@
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
-
+    //Initialize a variable to 1
+    let num = 1;
+    //use a while loop to iterate over a range of numbers (1 - 100)
+    while(num <= 100){
+        //use a conditional chain to print each number to the console, with the following exceptions,
+        //print "FizzBuzz" for every number that's a multiple of 3 & 5
+        if(num % 3 == 0 && num % 5 == 0){
+            console.log("FizzBuzz");
+        //print "Fizz" for every number that's a multiple of 3
+        }else if(num % 3 == 0){
+            console.log("Fizz");
+        //print "Buzz" for every number that's a multiple of 5
+        }else if(num % 5 == 0){
+            console.log("Buzz");
+        //print every number besides the conditions listed above, as it's appropriate number value
+        }else{
+            console.log(num);
+        }
+        num++;
+    }
 
     // YOUR CODE GOES ABOVE HERE //
 }
