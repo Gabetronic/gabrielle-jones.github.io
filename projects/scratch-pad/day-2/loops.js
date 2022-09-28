3f84fc7dec49bed3ed9fe1e3b47dcde7a041832f
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-09-28 19:14:27 +0000

    updated Scratch Pad D2

--- projects/scratch-pad/day-2/loops.js
+++ projects/scratch-pad/day-2/loops.js
@@ -13,7 +13,11 @@
 function printArrayValues(array) {
   // YOUR CODE BELOW HERE //
   
-  
+  //Use a for loop to iterate over the array parameter
+  for(var i = 0; i < array.length; i++){
+    //use the console.log() function to print each iteration to the console
+    console.log(array[i]);
+  }
   
   
   // YOUR CODE ABOVE HERE //
@@ -26,7 +30,11 @@ function printArrayValues(array) {
 function printArrayValuesInReverse(array) {
   // YOUR CODE BELOW HERE //
   
-  
+   //Use a for loop to iterate backwards over the array parameter
+   for(var i = array.length - 1; i >= 0; i--){
+    //use the console.log() function to print each iteration to the console
+    console.log(array[i]);
+   }
   
   
   // YOUR CODE ABOVE HERE //
@@ -39,10 +47,15 @@ function getObjectKeys(object) {
   // YOUR CODE BELOW HERE //
   
   //Initialize an empty array variable
+  let array = [];
   //Iterate through the object parameter with a for-in loop
-  //Push the object's keys into the array variable
-  //return the array
+  for(var key in object){
+    //Use the .push() method to place the object's keys into the array variable
+    array.push(key);
+  }
   
+  //return the array
+  return array;
   
   // YOUR CODE ABOVE HERE //
 }
@@ -54,9 +67,12 @@ function getObjectKeys(object) {
 function printObjectKeys(object) {
   // YOUR CODE BELOW HERE //
   
-  
-  
-  
+  //Iterate through the object parameter with a for-in loop
+  for(var key in object){
+    //Use console.log() to print the keys
+    console.log(key);
+  }
+
   // YOUR CODE ABOVE HERE //
 }
 
@@ -66,7 +82,15 @@ function printObjectKeys(object) {
 function getObjectValues(object) {
   // YOUR CODE BELOW HERE //
   
-  
+    //Initialize an empty array variable
+    let array = [];
+    //Iterate through the object parameter with a for-in loop
+    for(var key in object){
+      //Use the .push() method to place the object's values into the array variable
+      array.push(object[key]);
+    }
+  //return the array
+  return array;
   
   
   // YOUR CODE ABOVE HERE //
@@ -79,7 +103,11 @@ function getObjectValues(object) {
 function printObjectValues(object) {
   // YOUR CODE BELOW HERE //
   
-  
+   //Iterate through the object parameter with a for-in loop
+   for(var key in object){
+    //Use console.log() to print the values
+    console.log(object[key]);
+  }
   
   
   // YOUR CODE ABOVE HERE //
@@ -91,7 +119,16 @@ function printObjectValues(object) {
 function getObjectLength(object) {
   // YOUR CODE BELOW HERE //
   
-  
+    //Initialize an empty array variable
+    let array = [];
+    //Iterate through the object parameter with a for-in loop
+    for(var key in object){
+      //Use the .push() method to place the object's keys into the array variable
+      array.push(key);
+    }
+    
+    //return the array
+    return array.length;
   
   
   // YOUR CODE ABOVE HERE //
@@ -105,8 +142,18 @@ function printObjectValuesInReverse(object) {
   // YOUR CODE BELOW HERE //
   
   //Initialize an empty array variable
-  //Push the object values into an array using a for-in loop
-  //Loop over the array backwards to print the values in reverse
+  let array = [];
+  //Iterate through the object parameter with a for-in loop
+  for(var key in object){
+    //Use the .push() method to place the object's values into the array variable
+    array.push(object[key]);
+  }
+  //use a for loop to iterate backwards over the array
+  for(var i = array.length -1; i >= 0; i--){
+    //print the array's elements to the console using the console.log() method
+    console.log(array[i]);
+  }
+  
   
   
   // YOUR CODE ABOVE HERE //
