fca50a6d1085c705ece9817ec9fa09856544fad6
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-10-10 02:49:59 +0000

    updated Studes

--- studies/operators.js
+++ studies/operators.js
@@ -1,5 +1,14 @@
 /**
- * 
+ * OPERATORS:
+ * 0. An operator performs some action(operation) on a single, or on multiple data values(operands).  Operations produce a result. 
+ * There are several groups of operators: arithmetic, assignment, comparison, logical, unary, & ternary.
+ * 1. Assignment operators assign values to their left operand, based on the value of the right operand. These operators make code more
+ * coherent.
+ * 2. Arithmetic operators are used to perform math operations on numerical operands.
+ * 3. Comparison operators compare the left and right operands to return a Boolean value.
+ * 4. Logical operators
+ * 5. Unary operators
+ * 6. Ternary operators
  * 
  */
 
