c7fbcc19458904628d696cb927dc3ab860738483
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-10-20 23:26:24 +0000

    updated JS

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -4,8 +4,16 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 function triangles() {
+      //make 7 func calls to log a triangle: used a for loop and length prop
+    //starting position = empty string variable,
+    //stopping position = when the string's length <= a given num param
+    //iteration =  add a string to the existing string
+    /*
+  for(let string = "#"; string.length <= 5; string += "#"){
+    console.log(string);
+  }
+}*/
 
-}
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -19,13 +27,13 @@ function fizzBuzz() {
         //use a conditional chain to print each number to the console, with the following exceptions,
         //print "FizzBuzz" for every number that's a multiple of 3 & 5
         if(num % 3 == 0 && num % 5 == 0){
-            console.log("FizzBuzz");
+            console.log("fizzbuzz");
         //print "Fizz" for every number that's a multiple of 3
         }else if(num % 3 == 0){
-            console.log("Fizz");
+            console.log("fizz");
         //print "Buzz" for every number that's a multiple of 5
         }else if(num % 5 == 0){
-            console.log("Buzz");
+            console.log("buzz");
         //print every number besides the conditions listed above, as it's appropriate number value
         }else{
             console.log(num);
