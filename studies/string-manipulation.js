7f3abbd9ed84cbe4ff90ba6d0158c5c3bddd1762
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-10-24 23:48:00 +0000

    updated Studies

--- studies/string-manipulation.js
+++ studies/string-manipulation.js
@@ -19,30 +19,31 @@ let result = string2 + string; //=> This is an example
 result += "!";
 console.log(result); //=> This is an example!
 
-// 2. .concat() //
+// 2. concat() //
 // Has the same effect as the concatenation and assignment operator combo.
-result = string.concat(string2); //=> This is an example
+result = string.concat(string2); 
+console.log(result) //prints => This is an example
 
 // 3. methods that grab a portion of a string //
 // .substring()'s parameters reference a starting & ending index; the method grabs all the characters between those indices
-let substringExample = result.substring(0, 4); //=> This
+let substringExample = result.substring(0, 4); 
+console.log(substringExample) //prints => This
 
-// .slice() has the same logic as .substring, but it can also take in negative indices.
-let sliceExample = result.slice(-3, 6); //=> 
+// slice() has the same logic as .substring, but it can also take in negative indices.
+let sliceExample = result.slice(-3); 
+console.log(sliceExample) //prints => ple
 
-
-
-// 5. .Split() and .join() //
+// 5. split() and join() //
 // .Split() splits a string into a set of substrings and deposits them into an array; .join() does the opposite. Depending on the
 // the argument, thes two methods can split a string with characters or spaces.
 
-// 6. .replace() //
+// 6. replace() //
 
 
-// 7. .toUpperCase() and .toLowerCase() //
+// 7. toUpperCase() and toLowerCase() //
 // Both of these methods change the case of any alphabetical characters within the string
 
-// 8. .trim() //
+// 8. trim() //
 // Trim will take out all whitespace that exists before and after the string's text
 
 
