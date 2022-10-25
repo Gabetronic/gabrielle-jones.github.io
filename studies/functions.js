81a39dc38c998d09d5a6f5c0b62b78c63d586a24
Gabrielle Jones (she/her) <gabrielle.ec.jones@gmail.com>
2022-10-25 17:35:11 +0000

    updated S

--- studies/functions.js
+++ studies/functions.js
@@ -1,8 +1,21 @@
 /**
  * FUNCTIONS:
- * 
- * 
+ * 0. In computing terms, a function is a small program that receives input, acts on that data, and outputs the result of its 
+ * action(s).  Functions are both fundamental to JavaScript, and very popular because of their reusability and ability to 
+ * simplify code.
+ * 1. The actions inherent to functions are divided into two phases: declaration and invocation.  The declaration phase uses 
+ * specific syntax to form the outline of how the function will behave: what it will take in as input, what task(s) it will 
+ * perform on that input data, and what users can expect from its output(s). A function invocation, or call occurs when a
+ * user wants to implement the function on specific data; it also uses a set syntax to pass data, or argument(s) into the
+ * function body.
+ * 2. Depending on the programmer's intent, functions can: be named, or unnamed, aka anonymous; parameters & arguments can be
+ * optional; and functions can be attached to variables (these are called function expressions), or they can stand alone as
+ * function declarations.
+ * 3. The code inside a function body can access, modify and hold on to particular variables based on where they are located in 
+ * the coding environment and whether they are declared with var, let or const.
  */
+
+// 1. function declaration & 
 
 /**
  * Concepts - 
